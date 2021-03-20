befacba0f9993524de1bde190a4e09dd3a17479a
Romifd20 <ramoeblack06@gmail.com>
2021-03-20 18:47:05 +0600

    ciom

--- app/src/main/java/com/example/romi/LoginActivity.java
+++ app/src/main/java/com/example/romi/LoginActivity.java
@@ -1,37 +1,82 @@
 package com.example.romi;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 public class LoginActivity extends AppCompatActivity {
 
+    Button button;
+    EditText ed1, ed2;
+    TextView textView;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button bt = findViewById(R.id.fade);
-        findViewById(R.id.btn).setOnClickListener(v -> {});
-        if (!bt.getText().toString().equals("")){
-            Toast.makeText(this,"ЗДРАСТВУЙТЕ МАМАЙ",Toast.LENGTH_SHORT).show();
-        }
+        setContentView(R.layout.activity_login);
 
-        EditText et = findViewById(R.id.textView);
-        findViewById(R.id.button).setOnClickListener(v -> {
-            if (!et.getText().toString().equals("")){
-                Toast.makeText(this, "Добро пожаловать", Toast.LENGTH_SHORT).show();
+        button = findViewById(R.id.button);
+        ed1 = findViewById(R.id.textView);
+        ed2 = findViewById(R.id.tabMode);
+        ed1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() == 0 ) {
+                    button.setEnabled(false);
+                }
+            }
 
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.e("TextWatcherTest", "afterTextChanged:\t" + s.toString());
+            }
+        });
+        ed2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() == 0 ) {
+                    button.setEnabled(false);
+                } else {
+                    button.setEnabled(true);
+                }
+            }
 
+            @Override
+            public void afterTextChanged(Editable s) {
 
             }
         });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!ed1.getText().toString().isEmpty() && !ed2.getText().toString().isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "добро пожоловать", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Вы не заполнили поля", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        });
+
     }
 }
